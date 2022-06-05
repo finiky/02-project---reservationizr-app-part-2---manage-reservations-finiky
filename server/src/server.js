a136324b806e204662235cde752925ff9074d49b
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-06 00:26:04 +1200

    connected to mongooose by specifying the url in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT || 5001;
 const app = require("./app");
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/mongo");
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);
 });
