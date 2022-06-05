058bcffb54f4e328c17c91e113af7ba01cf73201
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-06 01:16:11 +1200

    created a mongoose model in  RestaurantModel.js

--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -1,1 +1,9 @@
-// FIXME: Add a Mongoose model here
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const RestaurantSchema = new Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    image: {type: String, required: true}
+});
+const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+module.exports = Restaurant;
