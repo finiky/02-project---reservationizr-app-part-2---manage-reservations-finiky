19437262b6e202bf7fd277a65c359a2a68c2eaa6
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-06 03:47:50 +1200

    created and tested a path for getting a list of restaurants. have not added celebrations/joi middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const RestaurantModel = require("./models/RestaurantModel");
-const ReservationModel = require("./models/ReservationModel");
+const formatRestaurant = require("./utils/formatRestaurants");
+//const ReservationModel = require("./models/ReservationModel");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/restaurants", async (requset, response) => {
   const restaurants = await RestaurantModel.find({});
-  response.status(201).send(restaurants);
+  const formattedRestaurants = restaurants.map( (restaurant) => formatRestaurant(restaurant));
+  response.status(200).send(formattedRestaurants);
 });
 
 module.exports = app;
