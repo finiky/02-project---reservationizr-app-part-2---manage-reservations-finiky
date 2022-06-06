e33ad5a5c2da42c6af389060fc013c6a8c9fe82e
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-06 18:26:23 +1200

    wrote the tests for unhappy aand happy paths for app.get of restaurant and single restaurant lists. also modified the app.get requests for them in app.js by removing the try catch

--- server/src/app.js
+++ server/src/app.js
@@ -11,40 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/restaurants", async (request, response, next) => {
-  try {
-    const restaurants = await RestaurantModel.find({});
-    const formattedRestaurants = restaurants.map((restaurant) =>
-      formatRestaurant(restaurant)
-    );
-    response.status(200).send(formattedRestaurants);
-  } catch (error) {
-    error.status = 500;
-    next(error);
-  }
+  const restaurants = await RestaurantModel.find({});
+  const formattedRestaurants = restaurants.map((restaurant) =>
+    formatRestaurant(restaurant)
+  );
+  response.status(200).send(formattedRestaurants);
 });
 
 app.get("/restaurants/:id", async (request, response, next) => {
-  try {
-    const { id } = request.params;
-    if (validId(id) === false) {
-      throw new Error("id provided is invalid");
-    } else {
-      try {
-        const restaurant = await RestaurantModel.findById(id);
-        if (undefined) {
-          throw new Error("id not found");
-        } else {
-          return response.status(200).send(formatRestaurant(restaurant));
-        }
-      } catch (error) {
-        error.status = 404;
-        next(error);
-      }
-    }
-  } catch (error) {
-    error.status = 400;
-    next(error);
+  const { id } = request.params;
+  if (validId(id) === false) {
+    return response.status(400).send({ message: "id provided is invalid" });
   }
+  const restaurant = await RestaurantModel.findById(id);
+  if (restaurant === null) {
+    return response.status(404).send({ message: "id not found" });
+  }
+  return response.status(200).send(formatRestaurant(restaurant));
 });
 app.use(errors());
 
--- server/src/app.test.js
+++ server/src/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("GET/app", () => {
-  it("aap.get('/restaurants'), should respond with a list of avaialbe restaurants", async () => {
+  test("aap.get('/restaurants'), should respond with a list of avaialbe restaurants", async () => {
     const expected = [
       {
         id: "616005cae3c8e880c13dc0b9",
@@ -32,5 +32,43 @@ describe("GET/app", () => {
         expect(response.body).toEqual(expected);
       })
       .expect(200);
+  });
+
+  test("app.get('/restaurants/:id'), should return a single restuarant object", async () => {
+    const expected = {
+      id: "616005cae3c8e880c13dc0b9",
+      name: "Curry Place",
+      description:
+        "Bringing you the spirits of India in the form of best authentic grandma's recipe dishes handcrafted with love by our chefs!",
+      image: "https://i.ibb.co/yftcRcF/indian.jpg",
+    };
+    await request(app)
+      .get("/restaurants/616005cae3c8e880c13dc0b9")
+      .expect((response) => {
+        expect(response.body).toEqual(expected);
+      })
+      .expect(200);
+  });
+
+  test("app.get('/restaurants/invalid-id'), should return a message saying id provided is invalid and a 400 status code", async () => {
+    const expected = { message: "id provided is invalid" };
+    await request(app)
+      .get("/restaurants/invalid-id")
+      .expect((response) => {
+        expect(response.body).toEqual(expected);
+      })
+      .expect(400);
+  });
+
+  test("app.get('/restaurants/:id'), should respond with a id not found and a 404 status", async () => {
+    const expected = {
+      message: "id not found",
+    };
+    await request(app)
+      .get("/restaurants/507f1f77bcf86cd799439015")
+      .expect((response) => {
+        expect(response.body).toEqual(expected);
+      })
+      .expect(404);
   });
 });
