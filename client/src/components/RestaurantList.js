d9239859aa85b7a70de2fcdfd033328e0a7d9492
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-09 01:13:28 +1200

    restaurantList component was completed on the client side to display a list of all the available restaurants

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -1,9 +1,38 @@
 import "./RestaurantList.css";
-
+import React, { useState, useEffect } from "react";
 const RestaurantList = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "http://localhost:5001/restaurants";
+      let data = await fetch(url);
+      data = await data.json();
+      setRestaurants(data);
+    };
+    fetchData();
+  }, []);
   return (
     <>
-      <h1>Restaurants</h1>
+      <h1 className="heading">Restaurants</h1>
+      <ul className="restaurants">
+        {restaurants.map(({ image, name, description, id }) => {
+          const link = `http://localhost:5001/restaurants/${id}`;
+          return (
+            <li key={id} className="restaurant">
+              <div className="imageContainer">
+                <img className="image" src={image} alt={name}></img>
+              </div>
+              <h2 className="name">{name}</h2>
+              <p className="description">{description}</p>
+              <button className="button">
+                <a className="link" href={link}>
+                  Reserve Now &rarr;
+                </a>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
