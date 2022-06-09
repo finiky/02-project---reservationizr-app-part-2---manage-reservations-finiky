e09d845955d61bd5aa4a62444745cb489948b6e1
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-09 18:32:05 +1200

    created a ReservationList react component to disoplay a list of reservations

--- client/src/components/ReservationList.js
+++ client/src/components/ReservationList.js
@@ -1,10 +1,38 @@
 import "./ReservationList.css";
 import { formatDate } from "../utils/formatDate";
+import React, { useState, useEffect } from "react";
 
 const ReservationList = () => {
+  const [reservations, setReservations] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchUrl = "http://localhost:5001/reservations";
+      const response = await fetch(fetchUrl);
+      const data = await response.json();
+      setReservations(data);
+    };
+    fetchData();
+  }, []);
+  if (reservations.length === 0) {
+    return <>
+      <p>You don't have any reservations</p>
+      <a href="http://localhost:3000">View the restaurants</a>
+    </>
+  }
   return (
     <>
       <h1>Upcoming reservations</h1>
+      <ul>
+        {reservations.map(({ restaurantName, date, id }) => {
+          return (
+            <li key={id}>
+              <h2>{restaurantName}</h2>
+              <p>{formatDate(date)}</p>
+              <a href={`http://localhost:3000/reservations/${id}`}>View details</a>
+            </li>
+          )
+        })}
+      </ul>
     </>
   );
 };
