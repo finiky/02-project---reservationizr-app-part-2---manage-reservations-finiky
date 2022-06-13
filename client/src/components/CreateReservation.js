c502c583582e36e935c9b0e01e133738e45692e1
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-13 22:31:28 +1200

    edited Createproperty to include the conditions when there is an error during creating

--- client/src/components/CreateReservation.js
+++ client/src/components/CreateReservation.js
@@ -1,31 +1,52 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import { useAuth0 } from "@auth0/auth0-react";
 import "react-datepicker/dist/react-datepicker.css";
 import "./CreateReservation.css";
 
-
 const CreateReservation = ({ restaurantName, id }) => {
   const [partySize, setPartySize] = useState(0);
   const [date, setDate] = useState(new Date());
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorStatus, setErrorStatus] = useState(false);
   const { getAccessTokenSilently } = useAuth0();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const accessToken = await getAccessTokenSilently();
     const reservation = { partySize, date, restaurantName };
-    fetch(`http://localhost:5001/restaurants/${id}`, {
+    const response = fetch(`http://localhost:5001/restaurants/${id}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${accessToken}`,
       },
       body: JSON.stringify(reservation),
-    }).then(() => {
-      navigate("/reservations");
     });
+
+    if(!response.ok) {
+      setIsError(true);
+      setErrorStatus(response.status);
+    }else {
+      setIsLoading(false);
+      navigate("/reservations");
+    }
   };
+  if (isError) {
+    return (
+      <>
+        <p className="no-reservation">
+          Error creating a reservation (error status {errorStatus})
+        </p>
+        <Link to={`/restaurants/${id}`}>
+          Return to restaurant
+        </Link>
+      </>
+    );
+  }
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -46,7 +67,7 @@ const CreateReservation = ({ restaurantName, id }) => {
           dateFormat="dd/MM/yyyy, h: mm aa"
           required
         />
-        <button>Submit</button>
+        <button disabled={isLoading}>Submit</button>
       </form>
     </>
   );
