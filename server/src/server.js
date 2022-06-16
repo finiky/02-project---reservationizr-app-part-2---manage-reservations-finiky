3674783822698f288df12abdb62441fdf4d991c9
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-06-16 16:29:27 +1200

    edited server.js for mongoDbUri

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,8 @@
 const port = process.env.PORT || 5001;
 const app = require("./app");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/mongo");
+const mongoDbUri = process.env.Mongo_URI || "mongodb://localhost:27017/mongo";
+mongoose.connect(mongoDbUri);
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);
 });
